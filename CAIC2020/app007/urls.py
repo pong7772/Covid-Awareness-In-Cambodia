e07076eef9df56dd3fc2315b3665cf35b1c414e9
Mengtongun <unmengtong@gmail.com>
2021-03-17 16:55:19 +0700

    Update

--- CAIC2020/app007/urls.py
+++ CAIC2020/app007/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home),
+    path('login/', views.login)
     # path('survey/', views.survey)
 ]
--- CAIC2020/app007/views.py
+++ CAIC2020/app007/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/main.html')
-
+def login(request):
+    return render(request, 'login/login.html')
 
 # Create your views here.
